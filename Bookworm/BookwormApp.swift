8e0912f3940ef1aa3f1c1b526ad6f19e3c336593
Jiaming Guo <j23guo@gmail.com>
2022-08-29 22:39:26 -0400

    Initial Commit

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Jiaming Guo on 2022-08-29.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
